df1a0eb6832abbd0e1aec2f2d92bc4e48e37c95d
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-24 10:09:12 -0300

    feat: rotas, jsx, css global and modularizado

---
+++ fundamentos/src/pages/estiloso.tsx
@@ -0,0 +1,8 @@
+import styles from "../styles/Estiloso.module.css";
+export default function Estiloso() {
+  return (
+    <div className={styles.roxo}>
+      <h1>Usando css Módules</h1>
+    </div>
+  );
+}
